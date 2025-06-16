81fe33d3dd34efec81aa1c252901ee30d0bead8b
rzastwn <rezastwn2004@gmail.com>
2025-06-16 16:58:44 +0700

    update css dan javascript

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -18,11 +18,11 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="<?= base_url('');?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url('');?>assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?= base_url('');?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?= base_url('');?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= base_url('');?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Main CSS File -->
   <link href="<?= base_url('assets/css/main.css') ?>" rel="stylesheet">
@@ -406,16 +406,16 @@ Data Analyst adalah seseorang yang bertanggung jawab dalam mengumpulkan, mengola
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/typed.js/typed.umd.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/aos/aos.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?= base_url('');?>assets/vendor/swiper/swiper-bundle.min.js"></script>
 
   <!-- Main JS File -->
   <script src="assets/js/main.js"></script>
